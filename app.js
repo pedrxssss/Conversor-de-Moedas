19aa0f606bdc98a98cc7fc2f11d806bfd2bfeb86
pedrxssss <pedro.sousa071100@gmail.com>
2023-11-04 15:24:29 -0300

    New features and styles

--- app.js
+++ app.js
@@ -1,34 +1,45 @@
 /* Javascript Conversor de moedas */
 //Moedas
 let valorDigitado = document.querySelector(".input");
+let valorConvertido = document.querySelector(".input-receive");
 let moedaSelecionada = document.getElementsByName("moedaEstrangeira");
 
 //Botoes
-let btnConverter = document.querySelector(".btnConverter");
 let btnLimpar = document.querySelector(".btnLimpar");
 
 //Instrucao
 let instrucao = document.querySelector(".paragrafo");
+let btnChangeName = document.querySelector(".changeButtonName");
 
 //Selecao de moedas
 let moedaEstrageira = "";
 let moedaConvertida = "";
 
 //API de conversão
-let url = 'https://economia.awesomeapi.com.br/json/last/'
-let coins = 'BRL-USD,BRL-GBP,BRL-EUR,BTC-BRL'
+let url = "https://economia.awesomeapi.com.br/json/last/";
+let coins = "BRL-USD,BRL-GBP,BRL-EUR,BTC-BRL";
 
 //Conversão do valor de real para o escolhido usando a API
-btnConverter.addEventListener('click', function () {
+btnLimpar.addEventListener("click", function () {
+  valorDigitado.focus();
+  valorDigitado.value = "";
+  valorConvertido.value = "";
+  moedaSelecionada[0].checked = true;
+  moedaSelecionada[1].checked = false;
+  moedaSelecionada[2].checked = false;
+  moedaSelecionada[3].checked = false;
+});
+
+function digiting() {
   fetch(url + coins)
     .then(function (response) {
-      return response.json()
+      return response.json();
     })
     .then(function (data) {
-      let realDolar = parseFloat(data.BRLUSD['bid'])
-      let realEuro = parseFloat(data.BRLEUR['bid'])
-      let realLibra = parseFloat(data.BRLGBP['bid'])
-      let realBtc = parseFloat(data.BTCBRL['bid'])
+      let realDolar = parseFloat(data.BRLUSD["bid"]);
+      let realEuro = parseFloat(data.BRLEUR["bid"]);
+      let realLibra = parseFloat(data.BRLGBP["bid"]);
+      let realBtc = parseFloat(data.BTCBRL["bid"]);
 
       real = parseFloat(valorDigitado.value);
 
@@ -41,62 +52,64 @@ btnConverter.addEventListener('click', function () {
 
       //Estrutura Switch Case
       switch (moedaEstrageira) {
-        case 'Dólar':
-          moedaConvertida = real * realDolar
-          mensagemFormatada(moedaConvertida.toLocaleString('en-US', { style: 'currency', currency: 'USD' }))
-          break
+        case "Dólar":
+          moedaConvertida = real * realDolar;
+          mensagemFormatada(
+            moedaConvertida.toLocaleString("en-US", {
+              style: "currency",
+              currency: "USD",
+            })
+          );
+          break;
 
-        case 'Euro':
-          moedaConvertida = real * realEuro
-          mensagemFormatada(moedaConvertida.toLocaleString('de-DE', { style: 'currency', currency: 'EUR' }))
-          break
+        case "Euro":
+          moedaConvertida = real * realEuro;
+          mensagemFormatada(
+            moedaConvertida.toLocaleString("de-DE", {
+              style: "currency",
+              currency: "EUR",
+            })
+          );
+          break;
 
-        case 'Líbra':
-          moedaConvertida = real * realLibra
-          mensagemFormatada(moedaConvertida.toLocaleString('en-GB', { style: 'currency', currency: 'GBP' }))
-          break
+        case "Líbra":
+          moedaConvertida = real * realLibra;
+          mensagemFormatada(
+            moedaConvertida.toLocaleString("en-GB", {
+              style: "currency",
+              currency: "GBP",
+            })
+          );
+          break;
 
-        case 'Bitcoin':
-          moedaConvertida = real / realBtc
-          mensagemFormatada(parseFloat(moedaConvertida).toFixed(5))
-          break
+        case "Bitcoin":
+          moedaConvertida = real / realBtc;
+          mensagemFormatada(parseFloat(moedaConvertida).toFixed(5));
+          break;
 
         default:
-          instrucao.textContent = 'Escolha uma moeda.'
-      } isNaN(moedaConvertida) ? moedaConvertida = 0 : ''
-    })
-})
-
-btnLimpar.addEventListener('click', function () {
-  valorDigitado.focus()
-  valorDigitado.value = ''
-  instrucao.textContent = 'Digite o valor, escolha a moeda e converta'
-  moedaSelecionada[0].checked = false
-  moedaSelecionada[1].checked = false
-  moedaSelecionada[2].checked = false
-  moedaSelecionada[3].checked = false
-})
-
-/* Mensagem de instrucao na tela */
-function mensagemFormatada(moedaConvertida) {
-  console.log(real)
-  instrucao.textContent = `O valor ${real.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })} convertido em ${moedaEstrageira} é ${moedaConvertida}`
+          instrucao.textContent = "Escolha uma moeda.";
+      }
+      isNaN(moedaConvertida) ? (moedaConvertida = 0) : "";
+    });
 }
 
-/* Verificador de valor digitado */
-//Bloquear botao
-function bloquearBotao() {
-  if (valorDigitado.value == 0 || valorDigitado == '' || valorDigitado == null) {
-    btnConverter.setAttribute('disabled', 'disabled')
-    btnConverter.style.background = '#ccc'
-    btnConverter.style.cursor = 'not-allowed'
+function changeName() {
+  for (let i = 0; i < moedaSelecionada.length; i++) {
+    if (moedaSelecionada[i].checked) {
+      btnChangeName.textContent = moedaSelecionada[i].value;
+    }
   }
 }
 
-//Ativar o botao
-function ativarBotao() {
-  if (valorDigitado.value > 0) {
-    btnConverter.removeAttribute('disabled')
-    btnConverter.style.cursor = 'pointer'
+/* Mensagem de instrucao na tela */
+function mensagemFormatada(moedaConvertida) {
+  valorConvertido.value = `${moedaConvertida}`;
+  valorConvertido.style.color = "black";
+  instrucao.textContent = "Valor convertido";
+
+  if (valorDigitado.value == "") {
+    valorConvertido.value = "";
+    instrucao.textContent = "Converta o valor";
   }
 }
